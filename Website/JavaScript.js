f6680dd8633a7bc0007923e7af7d01d2fec3cf12
SunnyGB <sunny.gb@live.com>
2015-11-05 16:56:10 +0000

    Abdullah Updated

--- Website/JavaScript.js
+++ Website/JavaScript.js
@@ -9,11 +9,28 @@ console.log(aray.length);
 var string="hello world thats a wonderful name";
 console.log(string.toUpperCase());
 console.log(string.split(" "));
-var object = document.getElementsByTagName("a");
-//document.onclick = function () {
-  //  alert("you have clicked");
-//}
-var obj= document.getElementsByClassName("logo");
-obj.onclick = function () {
-    alert("you have clicked image");
+
+function WinLoad(){
+    alert("Alert on window load");
 }
+window.onload = function () {
+    WinLoad();
+}
+//document.onclick=function(){
+  //  alert("You click Anything");
+//q}
+
+var obj= document.getElementById("logo");
+    obj.onclick = function () {
+        alert("you have clicked image");
+    }
+    var emailField = document.getElementById("inputTxt");
+    emailField.onfocus = function () {
+        if (emailField.value = "Your Email")
+            emailField.value = "";
+    };
+    emailField.onblur = function () {
+        if (emailField.value = "")
+            emailField.value = "Your Email";
+
+    };
