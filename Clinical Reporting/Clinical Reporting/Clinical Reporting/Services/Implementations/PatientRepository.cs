4c948d529acb192c891d2912fce4d798e4501082
SunnyGB <sunny.gb@live.com>
2016-04-09 22:09:50 +0000

    Did Sone Work on Patient Repository + Complete Work

--- Clinical Reporting/Clinical Reporting/Clinical Reporting/Services/Implementations/PatientRepository.cs
+++ Clinical Reporting/Clinical Reporting/Clinical Reporting/Services/Implementations/PatientRepository.cs
@@ -5,6 +5,7 @@ using Clinical_Reporting.Model;
 using Clinical_Reporting.Services;
 using System.Data.Entity;
 using System.Linq;
+using System.Transactions;
 
 namespace Clinical_Reporting.Services.Implementations
 {
@@ -16,13 +17,69 @@ namespace Clinical_Reporting.Services.Implementations
             _context.Patients.Add(patient);
             await _context.SaveChangesAsync();
             return patient;
-      
+
         }
 
-        public Task DeletePatientAsync(long patientID)
+        public async Task DeletePatientAsync(long patientID)
         {
-            return null;
+            using (TransactionScope scope = new TransactionScope())
+            {
+                var patient = _context.Patients.FirstOrDefault(x => x.PatientID == patientID);
+                var Biochemistry = _context.Patients.Include("Biochemistry").FirstOrDefault(x => x.PatientID == patientID);
+                var BloodGroup = _context.Patients.Include("BloodGroup").FirstOrDefault(x => x.PatientID == patientID);
+                var ELISA_HBSAG = _context.Patients.Include("ELISA_HBSAG").FirstOrDefault(x => x.PatientID == patientID);
+                var ELISA_HCV  = _context.Patients.Include("ELISA_HCV").FirstOrDefault(x => x.PatientID == patientID);
+                var ELISA_TB = _context.Patients.Include("ELISA_TB").FirstOrDefault(x => x.PatientID == patientID);
+                var Haematology = _context.Patients.Include("Haematology").FirstOrDefault(x => x.PatientID == patientID);
+                var SemenAnalysi = _context.Patients.Include("SemenAnalysi").FirstOrDefault(x => x.PatientID == patientID);
+                var Serology = _context.Patients.Include("Serology").FirstOrDefault(x => x.PatientID == patientID);
+                var UrineExamination = _context.Patients.Include("UrineExamination").FirstOrDefault(x => x.PatientID == patientID);
+                foreach (var item in Biochemistry.Biochemistries)
+                {
+                    _context.Biochemistries.Remove(item);
+                }
+                 foreach(var item in BloodGroup.BloodGroups) {
+                    _context.BloodGroups.Remove(item);
+                }
+                foreach (var item in BloodGroup.ELISA_HBSAG)
+                {
+                    _context.ELISA_HBSAG.Remove(item);
+                }
+
+                foreach (var item in BloodGroup.ELISA_HCV)
+                {
+                    _context.ELISA_HCV.Remove(item);
+                }
+
+
+                foreach (var item in BloodGroup.ELISA_TB)
+                {
+                    _context.ELISA_TB.Remove(item);
+                }
+
+                foreach (var item in BloodGroup.Haematologies)
+                {
+                    _context.Haematologies.Remove(item);
+                }
+                foreach (var item in BloodGroup.SemenAnalysis)
+                {
+                    _context.SemenAnalysis.Remove(item);
+                }
+                foreach (var item in BloodGroup.Serologies)
+                {
+                    _context.Serologies.Remove(item);
+                }
+                foreach (var item in BloodGroup.UrineExaminations)
+                {
+                    _context.UrineExaminations.Remove(item);
+                }
+                _context.Patients.Remove(patient);
+             await   _context.SaveChangesAsync();
+                scope.Complete();
+            }
+
         }
+    
 
         public async Task<List<Patient>> GetAllPatientsAsync()
         {
