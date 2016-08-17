34d5e4e46bdfc5598d314bc1ab8afb4c612b9bfe
SunnyGB <sunny.gb@live.com>
2016-08-17 13:57:44 +0000

    Work on Wrapper Classes using T4 template

---
+++ AdmissionAndResult/AdmissionAndResult/Services/TextTemplate1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using AdmissionAndResult.Model;
+using System.ComponentModel.DataAnnotations;
+using AdmissionAndResult.Services;
+
+namespace AdmissionAndResult.Model.Wrapper
+{
+  public partial class <PrivateImplementationDetails>{38771C08-2378-4D61-AC7C-FC8DD11AC39C}W : ValidateModelCommon
+  {
+	public <PrivateImplementationDetails>{38771C08-2378-4D61-AC7C-FC8DD11AC39C}W(<PrivateImplementationDetails>{38771C08-2378-4D61-AC7C-FC8DD11AC39C} <PrivateImplementationDetails>{38771C08-2378-4D61-AC7C-FC8DD11AC39C})
+	{
+	}
+  }
+}
